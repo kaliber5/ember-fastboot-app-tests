9b2660db9986dd4f15e6138b5d149c86960b0b09
simonihmig <ihmig@kaliber5.de>
2018-05-06 00:16:24 +0200

    Fix JSDOM API, add FastBoot test

---
+++ fastboot-tests/index-test.js
@@ -0,0 +1,20 @@
+/* eslint-env node, mocha */
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('index', function() {
+
+  it('renders', function() {
+    return this.visit('/')
+      .then(function(res) {
+        let $ = res.jQuery;
+        let response = res.response;
+
+        // add your real tests here
+        expect(response.statusCode).to.equal(200);
+        expect($('#title').text()).to.equal('Welcome to Ember');
+      });
+  });
+
+});
